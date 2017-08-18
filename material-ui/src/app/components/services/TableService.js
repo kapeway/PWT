43070424472b7edda996ddcae5bceb28a50a45ff
Unknown <kavinfranco19@gmail.com>
2017-08-18 23:37:25 +0530

    hooked up the close and reopen claim functionality in UI

--- material-ui/src/app/components/services/TableService.js
+++ material-ui/src/app/components/services/TableService.js
@@ -13,4 +13,28 @@ angular.module('app')
         callback(error);
         console.log('failed to fetch claims data',error);
     })  
-}}]);
+    };
+    this.closeClaim = function(claimToClose,callback){
+        $http.put('http://127.0.0.1:5000/api/claims/close/'+claimToClose)
+    .then(function (response){
+        callback(response);
+            console.log('claim closed',response);
+        },
+    function (error){
+        callback(error);
+        console.log('failed to close claim',error);
+    })  
+    };
+    this.reopenClaim = function(claimToReopen,callback){
+        $http.put('http://127.0.0.1:5000/api/claims/reopen/'+claimToReopen)
+    .then(function (response){
+        callback(response);
+            console.log('claim reopened',response);
+        },
+    function (error){
+        callback(error);
+        console.log('failed to reopen claim',error);
+    })  
+    };
+}
+]);
--- material-ui/src/app/controllers/TableController.js
+++ material-ui/src/app/controllers/TableController.js
@@ -11,9 +11,29 @@
     var vm = this;
 
     vm.tableData = [];
+
     vm.getDate=function(nanoSeconds){
       return moment(new Date(nanoSeconds)).format("DD-MMM-YYYY");
     };
+
+    vm.closeClaim=function(claimToClose){
+    tableService.closeClaim(claimToClose.sno,function(response) {
+        if(response.status===200) {
+          console.log(response);
+          claimToClose.claimStatus=1;
+        }
+    });
+    };
+
+    vm.reopenClaim=function(claimToReopen){
+      tableService.reopenClaim(claimToReopen.sno,function(response) {
+          if(response.status===200) {
+            console.log(response);
+            claimToReopen.claimStatus=0;
+          }
+      });
+    };
+
     tableService.getClaims(function(response) {
         if(response.status===200) {
           console.log(response);
